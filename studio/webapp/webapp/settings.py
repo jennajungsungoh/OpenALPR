99a60ec89b25f194262701e7e32021726cfca8b4
ahn.woosuk <ahn.woosuk@lge.com>
2022-06-27 10:38:35 +0900

    update activate script

--- studio/webapp/webapp/settings.py
+++ studio/webapp/webapp/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'alpr',
-    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -50,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
-
 ]
 
 ROOT_URLCONF = 'webapp.urls'
